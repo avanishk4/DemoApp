2aaa257a7ec2e30e131c088b4a8e4fa9921261f5
Avanish <kmravanish4@gmail.com>
2022-12-29 16:59:25 +0530

    First Commit

---
+++ DemoApp/src/demoapp.java
@@ -0,0 +1,15 @@
+
+public class demoapp {
+
+	public static void main(String[] args) {
+		int[] a= {10,20,30,40};
+		for (int ele:a) {
+			System.out.println(ele);
+		}
+
+	}
+	String name= "sachin";
+	int count =name.toUpperCase().length();
+	System.out.println(count);
+
+}
---
+++ DemoApp/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Avanish Kumar
+ *
+ */
+module DemoApp {
+}
